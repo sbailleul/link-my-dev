4d3742cb3b8ebe45a657ff4544061280bfe8f4c9
sbailleul <bailleul.sacha@gmail.com>
2022-11-21 23:32:40 +0100

    feat(poc): Create team request works

--- poc/api/src/collaboration/domain/commands.rs
+++ poc/api/src/collaboration/domain/commands.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize)]
 pub enum TeamCommand{
     Create{ team_id: String, name: String}
 }
--- poc/api/src/collaboration/web/mod.rs
+++ poc/api/src/collaboration/web/mod.rs
@@ -7,6 +7,7 @@ use cqrs_es::{CqrsFramework, Query};
 use postgres_es::{PostgresCqrs, PostgresEventRepository, PostgresViewRepository};
 use rocket::serde::json::Json;
 use rocket::{Build, Rocket, State};
+use serde::Deserialize;
 use sqlx::{Pool, Postgres};
 
 use crate::collaboration::bootstrap::CollaborationState;
@@ -15,15 +16,18 @@ use crate::collaboration::domain::team::Team;
 use crate::collaboration::store::postgres::configure;
 use crate::Config;
 
+#[derive(Deserialize, Debug)]
+struct CreateTeamRequest{team_id: String, name: String}
 #[post("/teams", data = "<cmd>")]
-fn create_team(cmd: Json<TeamCommand>, state: &State<CollaborationState>) -> &str {
-    state.cqrs.execute("", cmd.0);
+fn create_team(cmd: Json<CreateTeamRequest>, state: &State<CollaborationState>) -> &str {
+    dbg!(&cmd);
+    state.cqrs.execute("", TeamCommand::Create { team_id: cmd.0.team_id, name: cmd.0.name });
     "CQRS"
 }
 
 
 pub async fn add_collaboration(builder:   Rocket<Build>, config: &Config) -> Result<Rocket<Build>>{
     let collaboration_state = CollaborationState::new(config).await?;
-    Ok(builder.manage(collaboration_state))
+    Ok(builder.manage(collaboration_state).mount("/", routes![create_team]))
 }
 
